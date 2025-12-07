b4617a9cab94b3712c7ae2b3711740bd757324b7
androseryo <shreyashchawhan@gmail.com>
2025-12-07 11:18:28 +0530

    Python Project 1 - Brand Name Generator
    Python Project 1 - Brand Name Generator

---
+++ task.py
@@ -0,0 +1,13 @@
+#Welcome Screen
+print("--> Welcome to Python Brand Name Generator <--")
+
+#User Inputs
+user_city = input("What is your city name?\n")
+user_pet_name = input("What is your pet name?\n")
+
+#output
+print("The Brand name is:\n")
+print("'"+user_city +" "+user_pet_name+"'")
+
+#exit Screen
+print("--> Thank You for Using Python Brand Name Generator <--")
